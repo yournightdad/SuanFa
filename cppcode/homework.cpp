75fd0a06b185b9c7d13f9e632cb4c68ae4ad0231
yournightdad <1417772397@qq.com>
2025-11-25 00:13:37 +0800

    hello

--- cppcode/homework.cpp
+++ cppcode/homework.cpp
@@ -1,15 +1,7 @@
-procedure insertsort(n:integer);
-  var i,j:integer;
-  begin
-    for i:=2 to n do
-    begin
-      a[0]:=a[i];
-      j:=i-1;
-      while a[j]>a[0] do  
-      begin
-        a[j+1]:=a[j];
-        j:=j-1;
-      end;
-      a[j+1]:=a[0];
-    end;
-  end;
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, Homework!" << endl;
+    return 0;
+}
